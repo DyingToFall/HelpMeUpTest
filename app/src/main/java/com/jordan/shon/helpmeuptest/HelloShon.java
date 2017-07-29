a2c88b19df10a73872e67bd86e49d34138472a55
Matt Stuart <matt80112@gmail.com>
2017-07-29 12:41:05 -0700

    New Changes
    Here is what i did

---
+++ app/src/main/java/com/jordan/shon/helpmeuptest/HelloShon.java
@@ -0,0 +1,14 @@
+package com.jordan.shon.helpmeuptest;
+
+/**
+ * Created by Home on 7/29/2017.
+ */
+
+public class HelloShon
+{
+    //Help
+    String Shon = "w";
+    int u = 3;
+    Integer X = u;
+
+}
